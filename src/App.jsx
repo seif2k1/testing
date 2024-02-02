68cdd750fc15379858aba0762825a23c86e4ec88
seif2k1 <seif2kghost@gmail.com>
2024-02-02 23:53:44 +0100

    4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
