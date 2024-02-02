523862da7449ea47b4f6ba5458bb8c8ac09d095e
seif2k1 <seif2kghost@gmail.com>
2024-02-03 00:00:40 +0100

    8

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 
 function App() {
+  const navo = useNavigate();
   return (
     <>
-      <h1>seof eeipqsd,m,dqs,d,;</h1>
+      <h1 onClick={() => navo("/users")}>seof eeipqsd,m,dqs,d,;</h1>
       <Router>
         <Routes>
           <Route path="/users" element={<h1>seif</h1>} />
           <Route path="/products" element={<h1>Prodicts</h1>} />
         </Routes>
       </Router>
-      <Link to="/users">Users</Link>
     </>
   );
 }
