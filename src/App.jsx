effc37d2dddc5823b8ac8b9f9c080a6a62308aaf
seif2k1 <seif2kghost@gmail.com>
2024-02-02 23:57:04 +0100

    6

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Provider } from "react-redux";
-import { Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Provider>
-      seof,eddodsql;
+    <>
+      <h1>seof eeipqsd,m,dqs,d,;</h1>
       <Router>
         <Routes>
           <Route path="/users" element={<h1>seif</h1>} />
           <Route path="/products" element={<h1>Prodicts</h1>} />
         </Routes>
       </Router>
-    </Provider>
+    </>
   );
 }
 export default App;
